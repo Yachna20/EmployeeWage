2b755393e7343304d666ab492d684755830e3c6a
Yachna20 <thakreyachna20@gmail.com>
2022-12-04 19:53:42 +0530

    [Yachna]-Employee is Present or Absent

---
+++ EmployeeWageComputationProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int fulltime = 1;
+        Random randobj = new Random();
+        int checkPresent = randobj.Next(0, 2);
+        Console.WriteLine("Random number" + checkPresent);
+        if (checkPresent == fulltime)
+        {
+
+            Console.WriteLine("Employee is present");
+        }
+        else
+        {
+
+            Console.WriteLine("Employee is absent");
+        }
+    }
+}
+
