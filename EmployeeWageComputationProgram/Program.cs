1a8c528d7dd68cc100ffdc0218d8a5a3dab91fe9
Yachna20 <thakreyachna20@gmail.com>
2022-12-04 20:14:36 +0530

    [Yachna]- Calculate the Employee Wage

--- EmployeeWageComputationProgram/Program.cs
+++ EmployeeWageComputationProgram/Program.cs
@@ -2,20 +2,26 @@
 {
     static void Main(string[] args)
     {
-        int fulltime = 1;
+        int fulltime = 1, Emp_Rate_Per_Hr = 20, empHrs = 0, empwage = 0;
         Random randobj = new Random();
         int checkPresent = randobj.Next(0, 2);
         Console.WriteLine("Random number" + checkPresent);
         if (checkPresent == fulltime)
         {
+            empHrs = 8;
 
             Console.WriteLine("Employee is present");
         }
         else
         {
+            empHrs = 0;
 
             Console.WriteLine("Employee is absent");
         }
+        empwage = empHrs * Emp_Rate_Per_Hr;
+        Console.WriteLine("Employee wage:" + empwage);
     }
 }
+    
+
 
