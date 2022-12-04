7f4644b935e798340a6dac53ddefa52bb753638c
Yachna20 <thakreyachna20@gmail.com>
2022-12-04 21:10:44 +0530

    [Yachna]- Add Part time Employee and Wage

--- EmployeeWageComputationProgram/Program.cs
+++ EmployeeWageComputationProgram/Program.cs
@@ -2,15 +2,20 @@
 {
     static void Main(string[] args)
     {
-        int fulltime = 1, Emp_Rate_Per_Hr = 20, empHrs = 0, empwage = 0;
+        int fulltime = 1, Part_time = 2, Emp_Rate_Per_Hr = 20, empHrs = 0, empwage = 0;
         Random randobj = new Random();
-        int checkPresent = randobj.Next(0, 2);
+        int checkPresent = randobj.Next(0, 3);
         Console.WriteLine("Random number" + checkPresent);
         if (checkPresent == fulltime)
         {
             empHrs = 8;
 
             Console.WriteLine("Employee is present");
+        }
+        else if (checkPresent == Part_time)
+        {
+            empHrs = 4;
+            Console.WriteLine("Employee is present part-time");
         }
         else
         {
@@ -22,6 +27,10 @@
         Console.WriteLine("Employee wage:" + empwage);
     }
 }
-    
+
+
+
+
+
 
 
