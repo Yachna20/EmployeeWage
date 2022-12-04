5f5bc941edc4f30fe416289f1212b1d1e29ca630
Yachna20 <thakreyachna20@gmail.com>
2022-12-04 21:48:48 +0530

    [Yachna]- Calculate wages for a month

--- EmployeeWageComputationProgram/Program.cs
+++ EmployeeWageComputationProgram/Program.cs
@@ -3,28 +3,37 @@
     public const int part_time = 1;
     public const int Full_time = 2;
     public const int Emp_Rate_Per_Hr = 20;
+    public const int No_Of_Working_Days = 20;
     static void Main(string[] args)
     {
         int empHrs = 0;
-        int empwage = 0;
-        Random random = new Random();
-        int empCheck = random.Next(0, 3);
-        switch (empCheck)
+        int empWage = 0;
+        int totalEmpWage = 0;
+        for (int day = 0; day < No_Of_Working_Days; day++)
         {
-            case part_time:
-                empHrs = 4;
-                break;
-            case Full_time:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
-                break;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case part_time:
+                    empHrs = 4;
+                    break;
+                case Full_time:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * Emp_Rate_Per_Hr;
+            totalEmpWage += empWage;
+            Console.WriteLine("Employee wage: " + empWage);
         }
-        empwage = empHrs * Emp_Rate_Per_Hr;
-        Console.WriteLine("Employee wage:" + empwage);
+        Console.WriteLine("Total Emp Wage : " + totalEmpWage);
     }
 }
+
+
 
 
 
