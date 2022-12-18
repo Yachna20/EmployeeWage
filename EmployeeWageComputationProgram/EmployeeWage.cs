6a1f5666824fa9dfe7b025888ede7601dd53ebde
Yachna20 <thakreyachna20@gmail.com>
2022-12-18 15:56:23 +0530

    [Yachna]-Ability to save the total wage for each company

---
+++ EmployeeWageComputationProgram/EmployeeWage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProgram
+{
+    internal class EmployeeWage
+    {
+        
+        public const int part_time = 1;
+        public const int Full_time = 2;
+        private string company;
+        private int empRatePerHr;
+        private int totalEmpWage;
+        private int NoOfWorkingDays;
+        private int maxHrsPerMonth;
+        public EmployeeWage(string company, int empRatePerHr, int NoOfWorkingDays, int maxHrsPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHr = empRatePerHr;
+            this.NoOfWorkingDays = NoOfWorkingDays;
+            this.maxHrsPerMonth = maxHrsPerMonth;
+        }
+        public void computeEmpWage()
+        {
+            int empHrs = 0;
+            int totalWorkingDays = 0;
+            int totalEmpHrs = 0;
+            while (totalEmpHrs <= this.maxHrsPerMonth && totalWorkingDays < this.NoOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case part_time:
+                        empHrs = 4;
+                        break;
+                    case Full_time:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days:" + totalWorkingDays + "Emp Hrs :" + empHrs);
+            }
+
+            totalEmpWage = totalEmpHrs * empRatePerHr;
+            Console.WriteLine("Total Employee Wage: " + totalEmpWage);
+           
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for Company :" + this.company + " is: " + totalEmpWage;
+        }
+        
+        
+    }
+}
--- EmployeeWageComputationProgram/Program.cs
+++ EmployeeWageComputationProgram/Program.cs
@@ -1,46 +1,18 @@
-﻿class Program
-{
-    public const int part_time = 1;
-    public const int Full_time = 2;
-    public static int computeEmpWage(string company, int empRatePerHr, int NoOfWorkingDays, int maxHrsPerMonth)
-    {
-        int empHrs = 0;
-        int totalWorkingDays = 0;
-        int totalEmpHrs = 0;
-        while (totalEmpHrs <= maxHrsPerMonth && totalWorkingDays < NoOfWorkingDays)
-        {
-            totalWorkingDays++;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-            {
-                case part_time:
-                    empHrs = 4;
-                    break;
-                case Full_time:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
-            totalEmpHrs += empHrs;
-            Console.WriteLine("Days:" + totalWorkingDays + "Emp Hrs :" + empHrs);
-        }
+﻿namespace EmployeeWageComputationProgram;
 
-        int totalEmpWage = totalEmpHrs * empRatePerHr;
-        Console.WriteLine("Total Employee Wage: " + totalEmpWage);
-        return totalEmpWage;
-    }
+ public class Program
+ { 
     static void Main(string[] args)
     {
-        computeEmpWage("Infosys", 20, 20, 100);
-        computeEmpWage("Reliance", 40, 25, 100);
-    }
-}
+            EmployeeWage BigBasket = new EmployeeWage("BigBasket", 20, 20, 100);
+            EmployeeWage Jio = new EmployeeWage("Jio", 40, 25, 100);
+            BigBasket.computeEmpWage();
+            Console.WriteLine(BigBasket.toString());
+            Jio.computeEmpWage();
+            Console.WriteLine(Jio.toString());
 
-
-
+        }
+  }
 
 
 
